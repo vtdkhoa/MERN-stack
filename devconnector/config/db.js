33dbab8e1f60c0c3632e52cb2893dda4325edb08
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-11-10 10:57:34 +0700

    Initialize MongoDB connected

---
+++ devconnector/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log(chalk.bgBlue('MongoDB Connected...'))
+  } catch (error) {
+    console.log(chalk.bgRedBright(error.message))
+    // Exit process with failure
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
