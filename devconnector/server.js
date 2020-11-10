7fa3a4b122d63551b80fc54f9f9df15019a340df
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-11-10 12:10:15 +0700

    Handle register user

--- devconnector/server.js
+++ devconnector/server.js
@@ -3,14 +3,23 @@ const chalk = require('chalk')
 
 const connectDB = require('./config/db')
 
+// Routes
+const userRoute = require('./routes/apis/user')
+
 const app = express()
 
 // Connect Database
 connectDB()
 
+// Initialize Middleware
+app.use(express.json({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('API Server, start!')
 })
+
+// Define Routes
+app.use('/api/user', userRoute)
 
 const PORT = process.env.PORT || 5000
 
