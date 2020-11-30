70eeb3250e5a718e9ba49d1abfa6f44c3660f3e9
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-11-30 14:14:18 +0700

    Handle: Logout user and Authentication Navbar

--- devconnector/client/src/actions/auth.js
+++ devconnector/client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types'
 
 // Register User
@@ -92,4 +93,9 @@ export const login = (email, password) => async dispatch => {
 
     dispatch({ type: LOGIN_FAIL })
   }
+}
+
+// Logout
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT })
 }
--- devconnector/client/src/components/layouts/Navbar.component.jsx
+++ devconnector/client/src/components/layouts/Navbar.component.jsx
@@ -1,19 +1,59 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { logout } from '../../actions/auth'
 
-const Navbar = () => (
-  <nav className="navbar bg-dark">
-    <h1>
-      <Link to="/">
-        <i className="fas fa-code"></i> DevConnector
-      </Link>
-    </h1>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
     <ul>
-      <li><Link to="/">Developers</Link></li>
+      <li>
+        <Link onClick={logout} to="#!">
+          <i className="fas fa-sign-out-alt"></i>&nbsp;
+          <span className="hide-sm">Logout</span>
+        </Link>
+      </li>
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li><Link to="#!">Developers</Link></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
-  </nav>
-)
+  )
 
-export default Navbar
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> DevConnector
+        </Link>
+      </h1>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
+    </nav>
+  )
+}
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch(logout())
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Navbar)
