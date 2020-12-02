a1969672fc60e4f20e00db49f5a3ae555f9dea46
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-12-02 16:40:52 +0700

    Handle: User dashboard

--- devconnector/client/src/components/pages/dashboard/Dashboard.component.jsx
+++ devconnector/client/src/components/pages/dashboard/Dashboard.component.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../../actions/profile'
+import PropTypes from 'prop-types'
+import Spinner from '../../layouts/Spinner/Spinner.component'
 
-const Dashboard = () => {
-  return <div>Dashboard</div>
+function Dashboard({ getProfile, auth: { user }, profile: { currentProfile, loading } }) {
+  useEffect(() => {
+    getProfile()
+  }, [])
+
+  if (loading && currentProfile === null) {
+    return <Spinner />
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>&nbsp;
+        Welcome {user && user.name}
+      </p>
+      {
+        currentProfile !== null ? (
+          <Fragment>
+            <div className="my-2">
+              <button className="btn btn-danger">
+                <i className="fas fa-minus-circle"></i>&nbsp;
+                Delete my account
+              </button>
+            </div>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <p>You have not yet setup a profile, please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </Fragment>
+        )
+      }
+    </Fragment>
+  )
 }
 
-export default Dashboard
+Dashboard.propTypes = {
+  getProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProp = state => {
+  return {
+    auth: state.auth,
+    profile: state.profile
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getProfile: () => dispatch(getCurrentProfile())
+  }
+}
+
+export default connect(
+  mapStateToProp,
+  mapDispatchToProps
+)(Dashboard)
