602e4dd087ce4a6bedaee418f4d6c1fa5552f5c6
Khoa Vo <khoa.vo@s3corp.com.vn>
2020-12-17 17:51:16 +0700

    Fix DeepScan: add className and key props

--- devconnector/client/src/components/pages/posts/PostItem.component.jsx
+++ devconnector/client/src/components/pages/posts/PostItem.component.jsx
@@ -18,31 +18,31 @@ const PostItem = ({ post }) => {
   } = post
 
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
       <div>
         <Link to={`/profile/${user}`}>
-          <img class="round-img" src={avatar} alt="avatar"/>
+          <img className="round-img" src={avatar} alt="avatar"/>
           <h4>{author}</h4>
         </Link>
       </div>
       <div>
         <h2>{title}</h2>
-        <p class="my-1">{paragraph}</p>
-        <p class="post-date">Posted on {moment(date).format('L')}</p>
-        <p className="my-1">Tags:&nbsp;
-          {tags && tags.slice().map(tag => (
-            <span className="badge badge-success">{tag}</span>
+        <p className="my-1">{paragraph}</p>
+        <p className="post-date">Posted on {moment(date).format('L')}</p>
+        <p classNameName="my-1">Tags:&nbsp;
+          {tags && tags.slice().map((tag, index) => (
+            <span key={index} classNameName="badge badge-success">{tag}</span>
           ))}
         </p>
-        <button type="button" class="btn btn-light">
-          <i class="fas fa-thumbs-up"></i>&nbsp;
+        <button type="button" className="btn btn-light">
+          <i className="fas fa-thumbs-up"></i>&nbsp;
           <span>{likes.length}</span>
         </button>
-        <Link to={`/post/${_id}`} class="btn btn-primary">
-          Discussion <span class='comment-count'>{comments.length}</span>
+        <Link to={`/post/${_id}`} className="btn btn-primary">
+          Discussion <span className='comment-count'>{comments.length}</span>
         </Link>
-        <button type="button" class="btn btn-danger">
-          <i class="fas fa-times"></i>
+        <button type="button" className="btn btn-danger">
+          <i className="fas fa-times"></i>
         </button>
       </div>
     </div>
